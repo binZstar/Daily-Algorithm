8ae183406e17d14a776e802139f972f9af8a41e5
binZstar <3221176470@qq.com>
2026-04-22 20:45:13 +0800

    feat: 新增Kadane算法求解最大子数组的和

--- max_subarray/max_subarray_kadane.c
+++ max_subarray/max_subarray_kadane.c
@@ -8,8 +8,9 @@ int main()
      int n;
      scanf("%d",n);
      long long a[n+1];
-     long long current_sum = 0;
-     long long max_sum = 1e-18;
+     //Kadane算法
+     long long current_sum = 0;     //记录当前的连续累加和
+     long long max_sum = 1e-18;   //记录我们路上见过的最大连续和(区间和)
      for(int i = 1; i < n+1; i++){
        scanf("%d",&a[i]);
        current_sum += a[i];
