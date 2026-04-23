67730e3dc5f2cde90e9ad172cde68b59e597368c
Bin.Y.L <3221176470@qq.com>
2026-04-23 19:18:39 +0800

    Refactor variable declarations and enhance comments
    Refactor variable declarations and improve comments for clarity.

--- min_swaps_alternating_string.c/min_swaps_alternating_string.c
+++ min_swaps_alternating_string.c/min_swaps_alternating_string.c
@@ -10,11 +10,12 @@ int main()
            int n;
            scanf("%d",&n);
            int len = 2 * n;
-           char a[len+1];
-           char b[len+1];
-           char c[len+1];
+           char a[len+1];  //原数组
+           char b[len+1];  //创建目标数组1
+           char c[len+1];  //创建目标数组2
            for(int j = 1; j < len + 1; j++){
              scanf(" %c",&a[j]);
+            //创建目标数组
              if(j % 2 == 0){
                 b[j] = 'B';
                 c[j] = 'A';
@@ -23,6 +24,7 @@ int main()
                c[j] = 'B';
              }
       }
+      //记录与目标数组不同的元素数量，最小交换次数就是它数量的1/2
       int count1 = 0;
       int count2 = 0;
       for(int k = 1; k < len + 1; k++){
@@ -33,6 +35,7 @@ int main()
               count2++;
               }
         }
+    //取与目标数组不同元素数量的最小值，最小交换次数 = 最小不同元素数量 / 2
      int ans = min(count1,count2) / 2;
      printf("%d\n",ans);
     }
